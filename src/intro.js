4de5a5287107046c96324937fc47843a364abae4
Ryan Soklaski <rsoklaski@gmail.com>
2016-12-11 15:46:18 -0500

    Added Character-wise and word-wise text murmur.

--- src/intro.js
+++ src/intro.js
@@ -1,26 +1,7 @@
 var content = [
   'The sky above the port was the color of television, tuned to a dead channel.',
-  "`It's not like I'm using,' Case heard someone say, as he shouldered his way ",
-  "through the crowd around the door of the Chat. `It's like my body's developed",
-  "this massive drug deficiency.' It was a Sprawl voice and a Sprawl joke.",
-  'The Chatsubo was a bar for professional expatriates; you could drink there for',
-  'a week and never hear two words in Japanese.',
   '',
-  'Ratz was tending bar, his prosthetic arm jerking monotonously as he filled a tray',
-  'of glasses with draft Kirin. He saw Case and smiled, his teeth a webwork of',
-  'East European steel and brown decay. Case found a place at the bar, between the',
-  "unlikely tan on one of Lonny Zone's whores and the crisp naval uniform of a tall",
-  'African whose cheekbones were ridged with precise rows of tribal scars. `Wage was',
-  "in here early, with two joeboys,' Ratz said, shoving a draft across the bar with",
-  "his good hand. `Maybe some business with you, Case?'",
-  '',
-  'Case shrugged. The girl to his right giggled and nudged him.',
-  "The bartender's smile widened. His ugliness was the stuff of legend. In an age of",
-  'affordable beauty, there was something heraldic about his lack of it. The antique',
-  'arm whined as he reached for another mug.',
-  '',
-  '',
-  'From Neuromancer by William Gibson'
+  "The dog's name was pizza butt."
 ]
 
 var line = []
@@ -28,7 +9,7 @@ var line = []
 var wordIndex = 0
 var lineIndex = 0
 
-var wordDelay = 120
+var wordDelay = 55
 var lineDelay = 400
 
 export default class Intro {
@@ -40,11 +21,17 @@ export default class Intro {
     this.game.state.start('Gameplay')
   }
 
+  preload () {
+    this.game.load.audio('text', 'assets/text.wav')
+  }
+
   create () {
     this.text = this.game.add.text(32, 32, '', { font: '15px Arial', fill: '#19de65' })
 
     this.text.inputEnabled = true
     this.text.events.onInputDown.add(this.transition, this)
+
+    this.textsound = this.game.add.audio('text')
 
     this.nextLine()
   }
@@ -56,25 +43,20 @@ export default class Intro {
     }
 
     //  Split the current line on spaces, so one word per array element
-    line = content[lineIndex].split(' ')
+    line = content[lineIndex].split(' ')  // Word-wise murmur
+    // line = content[lineIndex]  // Character-wise murmur
 
     //  Reset the word index to zero (the first word in the line)
     wordIndex = 0
 
     //  Call the 'nextWord' function once for each word in the line (line.length)
-    this.game.time.events.repeat(wordDelay, line.length, this.nextWord, this)
+    this.game.time.events.repeat(wordDelay, line.length + 1, this.nextWord, this)
 
     //  Advance to the next line
     lineIndex++
   }
 
-  nextWord () {
-    //  Add the next word onto the text string, followed by a space
-    this.text.text = this.text.text.concat(line[wordIndex] + ' ')
-
-    //  Advance the word index to the next word in the line
-    wordIndex++
-
+  nextCharacter () {
     //  Last word?
     if (wordIndex === line.length) {
       //  Add a carriage return
@@ -82,6 +64,32 @@ export default class Intro {
 
       //  Get the next line after the lineDelay amount of ms has elapsed
       this.game.time.events.add(lineDelay, this.nextLine, this)
+    } else {
+      //  Add the next word onto the text string, followed by a space
+      this.text.text = this.text.text.concat(line[wordIndex])
+      if (line[wordIndex] !== ' ') {
+        this.textsound.play()
+      }
+
+      //  Advance the word index to the next word in the line
+      wordIndex++
+    }
+  }
+  nextWord () {
+    //  Last word?
+    if (wordIndex === line.length) {
+      //  Add a carriage return
+      this.text.text = this.text.text.concat('\n')
+
+      //  Get the next line after the lineDelay amount of ms has elapsed
+      this.game.time.events.add(lineDelay, this.nextLine, this)
+    } else {
+      //  Add the next word onto the text string, followed by a space
+      this.text.text = this.text.text.concat(line[wordIndex] + ' ')
+      this.textsound.play()
+
+      //  Advance the word index to the next word in the line
+      wordIndex++
     }
   }
 }
